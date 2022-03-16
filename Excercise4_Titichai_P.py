ca667e6dfecc14796f1b1283b307b0c54da0bb18
Tuinuii <tuyhappy@gmail.com>
2022-03-17 00:05:58 +0700

    Create Excercise4_Titichai_P.py

---
+++ Excercise4_Titichai_P.py
@@ -0,0 +1,10 @@
+fund_eng = 90  # Fundamental English
+gen_bus = 80 # General Business
+intro_com = 95 # Introduction to Computer System
+com_pro = 96 # Computer Programming
+
+print("--- Your Score---")
+print("Fundamental English : ",fund_eng )
+print("General Business : ",gen_bus)
+print("Introduction to Computer System : ",intro_com)
+print("Computer Programming : ",com_pro )
