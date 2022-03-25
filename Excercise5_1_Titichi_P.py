c77d48454302f5eeca479b90c22a01c910ab2a34
Tuinuii <tuyhappy@gmail.com>
2022-03-25 17:11:11 +0700

    Revert "Create Excercise5_1_Titichi_P.py"
    This reverts commit 7963410f258b6c3aad111aad02168e9b8d9cfc86.

--- Excercise5_1_Titichi_P.py
+++
@@ -1,6 +0,0 @@
-num1 = int(input("Number 1 : "))
-num2 = int(input("Number 2 : "))
-print(num1,"+",num2,"= ",num1 + num2 )
-print(num1 ,"-",num2,"= ",num1 - num2 )
-print(num1 ,"x",num2,"= ",num1 * num2 )
-print(num1 ,"/",num2,"= ",int(num1 / num2) )
