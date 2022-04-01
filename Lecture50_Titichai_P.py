34beef849ee8afef41a1a69534314c55e50f620c
Tuinuii <tuyhappy@gmail.com>
2022-04-01 16:54:56 +0700

    Create Lecture50_Titichai_P.py

---
+++ Lecture50_Titichai_P.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+def subNumber(x,y):
+    print(x-y)
+def multiNumber(x,y):
+    print(x*y)
+def divNumber(x,y):
+    print(x/y)
+
+addNumber(100,25)
+subNumber(100,25)
+multiNumber(100,25)
+divNumber(100,25)
